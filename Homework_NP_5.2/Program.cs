adb8215937c8250797289cb3f2e7eb72064ef311
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-22 21:22:45 +0300

    Добавьте файлы проекта.

---
+++ Homework_NP_5.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework_NP_5._2
+{
+    class Program
+    {
+        static HttpClient httpClient = new HttpClient();
+        static async Task Main()
+        {
+            Console.WriteLine("Введите url адрес страницы: ");
+            string url = Console.ReadLine();
+            // получаем ответ
+            using HttpResponseMessage response = await httpClient.GetAsync(url);
+            // получаем ответ
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+
+}
